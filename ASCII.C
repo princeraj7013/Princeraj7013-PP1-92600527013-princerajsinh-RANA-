e826b9b660d29de1d4c0541135e915cdbd44896a
princeraj7013 <princerajsinh.rana148149@marwadiuniversity.ac.in>
2026-08-14 11:02:42 +0530

    Add files via upload

---
+++ ASCII.C
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+      char x;
+      clrscr();
+      printf("\n enter any character :");
+      scanf("%c",&x);
+
+      printf("\ input character is :%c",x);
+      printf("\n ASCII cord is :%d",x);
+
+      getch();
+}
