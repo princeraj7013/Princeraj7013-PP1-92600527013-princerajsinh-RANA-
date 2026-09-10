29da14d1f30c68abc2006f1a7c86f3362d6413c4
princeraj7013 <princerajsinh.rana148149@marwadiuniversity.ac.in>
2026-09-10 14:44:15 +0530

    Add files via upload

---
+++ -3-9.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+
+       int x,y;
+       clrscr();
+
+       printf("\n enter the two number:");
+       scanf("%d%d", &x,&y);
+
+       if(y==0)
+       {
+		printf("\n Division by zero is not posible");
+       }
+       else if(x%y==0)
+       {
+		printf("\n is divisible by %d", x,y);
+       }
+       else
+       {
+	      printf("\n not divisible by %d", x,y);
+
+       }
+       getch();
+}
---
+++ 3-7.C
@@ -0,0 +1,27 @@
+#include<stido.h>
+#include<conio.h>
+void main()
+{
+   float price,total_purchase,discount,final_payment;
+   clrscr();
+   printef("\n enter price per item: ");
+   scanf("%f",&price);
+
+   printef("\n enter quantity of per item: ");
+   scanf("%d",&quantity);
+
+   if (totle_purchase>1000)
+   {
+       discount=total_purchase*0.15;
+   }
+   else
+   {
+      discount=totle_purchase*0.10;
+   }
+
+     final_payment=totle_purchase-discount;
+
+     printf("\n totle purchase:rs %.2f",totle_purchase);
+     printf("\n totle discount:rs %.2f",discount);
+      printf("\n totle :rs %.2f",)
+
